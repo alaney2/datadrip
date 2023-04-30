d995fc05b3bd4a6e58849c3dcd7e7e9496590080
Alan Yao <alanyao.training@gmail.com>
2023-04-30 15:03:00 -0500

    contact us button

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import path from 'path';
 import fs from 'fs';
@@ -8,24 +8,65 @@ import Button from '@mui/material/Button';
 
 const filename = 'about.md';
 export default function MarkdownPage({ markdownContent }) {
+  const [fixedButton, setFixedButton] = useState(false);
+  const [buttonHeight, setButtonHeight] = useState('0');
+
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!buttonRef.current) return;
+
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const currentScrollPosition = window.scrollY;
+
+      if (!fixedButton && currentScrollPosition + windowHeight >= documentHeight) {
+        setFixedButton(true);
+        console.log('Button position fixed');
+      }
+    };
+
+    if (buttonRef.current) {
+      setButtonHeight(buttonRef.current.offsetHeight + 'px');
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [fixedButton]);
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', mb: 16 }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
       <Box sx={{ minHeight: '100vh' }}>
         <PageContent content={markdownContent} filename={filename} leftSidebar={false} showLastUpdated={false} />
       </Box>
 
       <Box
-        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw',  mb: 8 }}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100vw',
+          my: 4,
+          position: 'relative',
+          // height: '20vh',
+          height: buttonHeight,
+        }}
       >
+        <Box sx={{ width: '100%', height: fixedButton ? '20vh' : '0' }} />
         <Link href="/contact" passHref>
           <Button
+            ref={buttonRef}
             variant="contained"
             color="secondary"
             sx={{
-              mt: 12,
-              height: '20vh',
               width: '75vw',
-              position: 'relative',
+              position: fixedButton ? 'fixed' : 'absolute',
+              bottom: fixedButton ? '5rem' : '0',
+              left: '50%',
+              transform: 'translateX(-50%)',
               fontSize: '5vw',
               padding: 4,
               textTransform: 'capitalize',
@@ -33,7 +74,7 @@ export default function MarkdownPage({ markdownContent }) {
               border: '1.5vw solid',
               borderColor: 'primary.main',
               '&:hover': {
-                transform: 'scale(1.20)',
+                transform: 'translateX(-50%) scale(1.10)',
                 backgroundColor: 'secondary.main',
                 borderColor: 'primary.main',
                 textDecoration: 'none',
@@ -47,7 +88,7 @@ export default function MarkdownPage({ markdownContent }) {
       </Box>
     </Box>
   );
-};
+}
 
 export async function getStaticProps() {
   const filePath = path.join(process.cwd(), 'data', filename);
