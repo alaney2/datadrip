f6602e46ae67394a92c0e1bc3eb94e1c4324e762
Alan Yao <alanyao.training@gmail.com>
2023-05-02 21:33:43 -0500

    different node sizes in graph:

--- components/ForceGraphUrl.js
+++ components/ForceGraphUrl.js
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from 'react';
 import ForceGraph3D from '3d-force-graph';
 import { useTheme } from '@mui/material/styles';
+import SpriteText from 'three-spritetext';
+import * as THREE from 'three';
 
 
 const ForceGraphUrl = ({ data }) => {
@@ -13,7 +15,6 @@ const ForceGraphUrl = ({ data }) => {
   } else {
     backgroundColor = '#8e8e8e';
   }
-  
   
 
   function getNodeById(nodes, id) {
@@ -40,78 +41,122 @@ const ForceGraphUrl = ({ data }) => {
     
     const highlightNodes = new Set();
     const highlightLinks = new Set();
+    const clickedNodes = new Set();
+
     let hoverNode = null;
 
-    function updateHighlight() {
-      Graph
-      .nodeColor(Graph.nodeColor())
-      .linkWidth(Graph.linkWidth())
-      .linkDirectionalParticles(Graph.linkDirectionalParticles());
-    }
-    
+    const minDegree = Math.min(...data.nodes.map(n => n.degree));
 
     const Graph = ForceGraph3D()
       (containerRef.current)
       .graphData(data)
       .nodeLabel('id')
       .backgroundColor(backgroundColor)
+      .height('100px')
       .nodeAutoColorBy('group')
-      // .linkAutoColorBy(d => data.nodes[d.source].group)
-      // .linkAutoColorBy(d => getNodeById(data.nodes, d.source).group)
+
       .linkAutoColorBy(d => {
         const sourceNode = getNodeById(data.nodes, d.source);
         return sourceNode ? sourceNode.group : null;
       })
+
       .onNodeClick(node => {
         // Aim at node from outside it
-        const distance = 300;
-        const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+        // const distance = 690;
+        // const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
 
-        const newPos = node.x || node.y || node.z
-          ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
-          : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+        // const newPos = node.x || node.y || node.z
+        //   ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
+        //   : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
 
-        Graph.cameraPosition(
-          newPos, // new position
-          node, // lookAt ({ x, y, z })
-          2000  // ms transition duration
-        );
+        // Graph.cameraPosition(
+        //   newPos, // new position
+        //   node, // lookAt ({ x, y, z })
+        //   2500  // ms transition duration
+        // );
+
+        if ((!node && !highlightNodes.size) || (node && hoverNode === node)) return;
+
+        const clickedGroup = Graph.scene().getObjectByName(`NodeGroup_${node.id}`);
+
+        if (clickedNodes.has(node)) {
+          // Unhighlight neighbors and hide text
+          node.neighbors.forEach(neighbor => {
+            highlightNodes.delete(neighbor);
+          });
+          node.links.forEach(link => highlightLinks.delete(link));
+          clickedNodes.delete(node);
+          clickedGroup.sprite.visible = false;
+        } else {
+          // Highlight neighbors
+          node.neighbors.forEach(neighbor => {
+            highlightNodes.add(neighbor);
+          });
+          node.links.forEach(link => highlightLinks.add(link));
+          clickedNodes.add(node);
+          clickedGroup.sprite.visible = true;
+        }
+
+        hoverNode = node || null;
+        Graph
+          .nodeColor(Graph.nodeColor())
+          .linkWidth(Graph.linkWidth())
+          .linkDirectionalParticles(Graph.linkDirectionalParticles());
       })
-      // .nodeColor(node => highlightNodes.has(node) ? node === hoverNode ? 'rgb(255,0,0,1)' : 'rgba(255,160,0,0.8)' : 'rgba(0,255,255,0.6)')
+
+      .nodeThreeObject(node => {
+        // Create a group to hold the sphere and the label
+        const group = new THREE.Group();
+        group.name = `NodeGroup_${node.id}`;
+
+        const sizeFactor = 7; 
+        const sphereSize = Math.max(sizeFactor * Math.log10(node.degree - minDegree + 1), 1);
+        const sphereGeometry = new THREE.SphereGeometry(sphereSize);
+        const sphereMaterial = new THREE.MeshLambertMaterial({ 
+          color: node.color, 
+          transparent: true,
+          opacity: 0.75 
+        });
+        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+        group.add(sphere);
+
+        const sprite = new SpriteText(node.id);
+        sprite.material.depthWrite = false; // make sprite background transparent
+        sprite.color = node.color;
+        sprite.textHeight = 8;
+        sprite.visible = clickedNodes.has(node); // set visibility based on whether the node is clicked
+        group.add(sprite);
+        // Store the sprite as a property of the group for easier access later
+        group.sprite = sprite;
+        return group;
+      })
+      
       .linkWidth(link => highlightLinks.has(link) ? 4 : 1)
       .linkDirectionalParticles(link => highlightLinks.has(link) ? 4 : 0)
       .linkDirectionalParticleWidth(4)
       .linkDirectionalParticleSpeed(0.005)
-      .onNodeHover(node => {
-        // no state change
-        if ((!node && !highlightNodes.size) || (node && hoverNode === node)) return;
 
-        highlightNodes.clear();
-        highlightLinks.clear();
-        if (node && node.neighbors) {
-          highlightNodes.add(node);
-          console.log(node)
-          node.neighbors.forEach(neighbor => highlightNodes.add(neighbor));
-          node.links.forEach(link => highlightLinks.add(link));
-        }
+      // .onNodeHover(node => {
+      //   if ((!node && !highlightNodes.size) || (node && hoverNode === node)) return;
 
-        hoverNode = node || null;
+      //   highlightNodes.clear();
+      //   highlightLinks.clear();
+      //   if (node && node.neighbors) {
+      //     highlightNodes.add(node);
+      //     console.log(node)
+      //     node.neighbors.forEach(neighbor => highlightNodes.add(neighbor));
+      //     node.links.forEach(link => highlightLinks.add(link));
+      //   }
 
-
-        updateHighlight();
-      })
-      .onLinkHover(link => {
-        highlightNodes.clear();
-        highlightLinks.clear();
-
-        if (link) {
-          highlightLinks.add(link);
-          highlightNodes.add(link.source);
-          highlightNodes.add(link.target);
-        }
-        updateHighlight();
-      })
+      //   hoverNode = node || null;
+      //   Graph
+      //     .nodeColor(Graph.nodeColor())
+      //     .linkWidth(Graph.linkWidth())
+      //     .linkDirectionalParticles(Graph.linkDirectionalParticles())
+      // })
       ;
+
+      // Graph.onEngineStop(() => Graph.zoomToFit(400));
 
   }, [containerRef, data, backgroundColor]);
 
--- pages/graph.js
+++ pages/graph.js
@@ -8,52 +8,18 @@ import Box from '@mui/material/Box';
 import wikiConnections from '@/wiki-connections.json';
 const ForceGraph = dynamic(() => import('../components/ForceGraph'), { ssr: false });
 const ForceGraphUrl = dynamic(() => import('../components/ForceGraphUrl'), { ssr: false });
-import miserables from '@/miserables.json';
 
 const Graphology = require('graphology');
 const louvain = require('graphology-communities-louvain');
 
-function clusterTopicsWithLinks(nodes, links) {
-  // const graph = new Graphology();
-  const graph = new Graphology({ multi: true });
-
-  const nodeIndexLookup = {};
-  nodes.forEach((node, index) => {
-    nodeIndexLookup[node.id] = index;
-  });
-
-  const updatedLinks = links.map(({ source, target }) => ({
-    source: nodeIndexLookup[source],
-    target: nodeIndexLookup[target],
-  }));
-  
-  nodes.forEach((node, index) => {
-    graph.addNode(index, { label: node.id });
-  });
-
-  updatedLinks.forEach(({ source, target }) => {
-    graph.addEdge(source, target);
-  });
-
-  // Run the Louvain algorithm to find communities (clusters)
-  const communities = louvain(graph);
-
-  nodes.forEach((node, index) => {
-    node.group = communities[index];
-  });
-
-}
-
-function transformData(inputData) {
+function clusterAndTransformTopics(inputData) {
   let nodes = [];
   let links = [];
-
   const edgeSet = new Set();
 
   for (const key in inputData) {
     const item = inputData[key];
-    nodes.push({ id: item.title });
-
+    nodes.push({ id: item.title, degree: 0 });
     item.prerequisites.forEach(prerequisiteKey => {
       if (inputData[prerequisiteKey]) {
         const source = inputData[prerequisiteKey].title;
@@ -62,7 +28,7 @@ function transformData(inputData) {
 
         if (!edgeSet.has(edgeKey)) {
           edgeSet.add(edgeKey);
-          links.push({ source, target });
+          links.push({ source, target, rotation: 0.8 });
         }
       }
     });
@@ -75,19 +41,61 @@ function transformData(inputData) {
 
         if (!edgeSet.has(edgeKey)) {
           edgeSet.add(edgeKey);
-          links.push({ source, target });
+          links.push({ source, target, rotation: 0.8 });
         }
       }
     });
   }
+
+  const graph = new Graphology({ multi: true });
+
+  const nodeIndexLookup = {};
+  nodes.forEach((node, index) => {
+    nodeIndexLookup[node.id] = index;
+  });
+
+  const updatedLinks = links.map(({ source, target }) => ({
+    source: nodeIndexLookup[source],
+    target: nodeIndexLookup[target],
+  }));
+
+  nodes.forEach((node, index) => {
+    graph.addNode(index, { label: node.id });
+  });
+
+  updatedLinks.forEach(({ source, target }) => {
+    graph.addEdge(source, target);
+  });
+  
+  graph.forEachNode((node, attributes, index) => {
+    const nodeId = attributes.label;
+    nodes[nodeIndexLookup[nodeId]].degree = graph.degree(node);
+  });
+  // Remove unconnected nodes based on their degree in the graph
+  graph.forEachNode((node, attributes, index) => {
+    if (graph.degree(node) === 0) {
+      graph.dropNode(node);
+      delete nodeIndexLookup[attributes.label];
+    }
+  });
+
+
+  // Update nodes array to only include connected nodes
+  nodes = nodes.filter(node => nodeIndexLookup.hasOwnProperty(node.id));
+  // Run the Louvain algorithm to find communities (clusters)
+  const communities = louvain(graph);
+  nodes.forEach((node, index) => {
+    node.group = communities[nodeIndexLookup[node.id]];
+  });
+  // Update links array to only include links with connected nodes
+  links = links.filter(link => nodeIndexLookup.hasOwnProperty(link.source) && nodeIndexLookup.hasOwnProperty(link.target));
+
   return { nodes, links };
 }
 
 
 export default function Graph() {
-  const transformedData = transformData(wikiConnections);
-
-  clusterTopicsWithLinks(transformedData.nodes, transformedData.links);
+  const transformedData = clusterAndTransformTopics(wikiConnections);
 
   return (
     <>
@@ -95,7 +103,7 @@ export default function Graph() {
         <title>Graph</title>
       </Head>
       <main>
-        <Box sx={{ height: '100vh' }}>
+        <Box sx={{ }}>
           <NavBar />
           {/* <ForceGraph data={data} /> */}
           <ForceGraphUrl data={transformedData} />
