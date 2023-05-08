b8e740edbe732c0f0f8772f37d0875396a7857a5
Alan Yao <alanyao.training@gmail.com>
2023-05-08 16:10:57 -0500

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@/components/ThemeContext';
 import { ReadingListProvider } from '@/components/ReadingListContext';
 import { NavBarVisibilityProvider } from '@/components/NavBarVisibilityContext';
 import { Analytics } from '@vercel/analytics/react';
-
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -13,6 +13,15 @@ export default function App({ Component, pageProps }) {
       <ThemeProvider>
         <ReadingListProvider>
           <NavBarVisibilityProvider>
+            <Script id="google-tag-manager" strategy="afterInteractive">
+              {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','G-CF4FCRS2YB');
+              `}
+            </Script>
             <CssBaseline enableColorScheme />
             <Component {...pageProps} />
           </NavBarVisibilityProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, Script } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
@@ -10,6 +10,12 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-CF4FCRS2YB"
+              height="0" width="0" style="display: none; visibility: hidden;" />`,
+            }}
+          />
         </body>
       </Html>
     );
