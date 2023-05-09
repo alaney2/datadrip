de06e746e1d223662aec5addeafac860e6e047bf
Alan Yao <alanyao.training@gmail.com>
2023-05-08 22:16:59 -0500

    gtag again again

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/components/ThemeContext';
 import { ReadingListProvider } from '@/components/ReadingListContext';
 import { NavBarVisibilityProvider } from '@/components/NavBarVisibilityContext';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -14,6 +15,21 @@ export default function App({ Component, pageProps }) {
           <NavBarVisibilityProvider>
             <CssBaseline enableColorScheme />
             <Component {...pageProps} />
+            <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-CF4FCRS2YB"/>
+            <Script
+              id='google-analytics'
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-CF4FCRS2YB', {
+                  page_path: window.location.pathname,
+                  });
+                `,
+                }}
+            />
           </NavBarVisibilityProvider>
         </ReadingListProvider>
       </ThemeProvider>
